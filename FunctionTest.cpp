a32e309a9b5946fbce94b6ffa297224d6ff3982d
KJC <kjc.git@gmail.com>
2013-05-12 19:45:35 -0400

    1st Function Tested

--- FunctionTest.cpp
+++ FunctionTest.cpp
@@ -5,6 +5,18 @@
 #include "BasicFunctions.h"
 #include <iostream>
 
+#include <boost/mpl/for_each.hpp>
+
+#ifdef FOR_PRINTING
+	struct printer
+	{
+		template < typename T > void operator()( T )
+		{
+			std::cout << T::toString() << std::endl;
+		}
+	};
+#endif
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	Identity< _a > a;
@@ -12,7 +24,19 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	std::cout << "a(12.2) = " << a(12.2) << std::endl;
 
-	auto f2 = a.operator+< Identity<_b> > (b);
+	auto f2 = a + b;
+
+	Param< _a, _b > pab(1.4, 2.5);
+	Param< _a > pa(1.2);
+	Param< _a > pa2(pab);
+
+	std::cout << f2.toString() << f2(pab) << std::endl;
+
+	typedef ParamCombiner< Param<_a>, Param<_b> >::types types;
+
+#ifdef FOR_PRINTING
+	mpl::for_each< types >( printer() );
+#endif
 	
 	return 0;
 }
